1ff5e86d0c06ba3ce2d2e90502cf3a3fc32aa01b
Sam <sismail1@ggc.edu>
2024-02-14 11:24:55 -0500

    Initial commit

---
+++ src/Bread.java
@@ -0,0 +1,86 @@
+public class Bread {
+    protected String breadName;
+    protected double flour, water, salt, sugar, bakingPowder, yeast;
+
+    public Bread(String breadName, double flour, double water, double salt, double sugar, double bakingPowder, double yeast){
+        this.breadName = breadName;
+        this.flour = flour;
+        this.water = water;
+        this.salt = salt;
+        this.sugar = sugar;
+        this.bakingPowder = bakingPowder;
+        this.yeast = yeast;
+    }
+
+    public String getBreadName() {
+        return breadName;
+    }
+
+    public void setBreadName(String breadName) {
+        this.breadName = breadName;
+    }
+
+    public double getFlour() {
+        return flour;
+    }
+
+    public void setFlour(double flour) {
+        this.flour = flour;
+    }
+
+    public double getWater() {
+        return water;
+    }
+
+    public void setWater(double water) {
+        this.water = water;
+    }
+
+    public double getSalt() {
+        return salt;
+    }
+
+    public void setSalt(double salt) {
+        this.salt = salt;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+
+    public double getBakingPowder() {
+        return bakingPowder;
+    }
+
+    public void setBakingPowder(double bakingPowder) {
+        this.bakingPowder = bakingPowder;
+    }
+
+    public double getYeast() {
+        return yeast;
+    }
+
+    public void setYeast(double yeast) {
+        this.yeast = yeast;
+    }
+    public String getIngredients() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Bread{" +
+                "breadName='" + breadName + '\'' +
+                ", flour=" + flour +
+                ", water=" + water +
+                ", salt=" + salt +
+                ", sugar=" + sugar +
+                ", bakingPowder=" + bakingPowder +
+                ", yeast=" + yeast +
+                '}';
+    }
+}
