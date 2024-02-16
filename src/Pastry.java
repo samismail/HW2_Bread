f94818efd1e5674344e618deecdcae55e106d02b
Sam <sismail1@ggc.edu>
2024-02-15 22:02:34 -0500

    update

---
+++ src/Pastry.java
@@ -0,0 +1,16 @@
+public class Pastry extends Bread{
+
+    public Pastry(String breadName, double flour, double water, double salt, double sugar, double bakingPowder, double yeast) {
+        super(breadName, flour, water, salt, sugar, bakingPowder, yeast);
+    }
+
+    @Override
+    public String getIngredients() {
+        return super.getIngredients();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
